548fbb67e37d69d1c6b3e3548bafad7cee3dc8c2
Scott Foltz <sfoltz1@carthage.edu>
2016-08-26 02:48:42 -0500

    navigation items and header added

--- app/src/main/java/sfoltz/navigationviewdemo/MainActivity.java
+++ app/src/main/java/sfoltz/navigationviewdemo/MainActivity.java
@@ -1,17 +1,54 @@
 package sfoltz.navigationviewdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private NavigationView mMainDrawer;
+    private ActionBar mActionBar;
+    private DrawerLayout mDrawerLayout;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mActionBar = getSupportActionBar();
+        mActionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.navigation_drawer_layout);
+
+
+        mMainDrawer = (NavigationView)findViewById(R.id.main_drawer);
+        if(mMainDrawer != null){
+            setupNavigationDrawerContent(mMainDrawer);
+        }
+
+        setupNavigationDrawerContent(mMainDrawer);
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -25,13 +62,60 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void setupNavigationDrawerContent(NavigationView navigationView) {
+        mMainDrawer.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                Fragment fragment = new Fragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                mTextView = (TextView)findViewById(R.id.textView);
+                switch (menuItem.getItemId()) {
+                    case R.id.home:
+                        menuItem.setChecked(true);
+                        mTextView.setText(menuItem.getTitle());
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        return true;
+                    case R.id.about:
+                        menuItem.setChecked(true);
+                        mTextView.setText(menuItem.getTitle());
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        return true;
+                    case R.id.skills:
+                        menuItem.setChecked(true);
+                        mTextView.setText(menuItem.getTitle());
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        return true;
+                    case R.id.projects:
+                        menuItem.setChecked(true);
+                        mTextView.setText(menuItem.getTitle());
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        return true;
+                    case R.id.work:
+                        menuItem.setChecked(true);
+                        mTextView.setText(menuItem.getTitle());
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        return true;
+                    case R.id.social:
+                        menuItem.setChecked(true);
+                        mTextView.setText(menuItem.getTitle());
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        return true;
+                }
+                return true;
+            }
+        });
     }
 }
